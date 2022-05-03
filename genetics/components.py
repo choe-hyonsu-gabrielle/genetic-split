4ad41182a5bce58fcbaf51f132e0b2c87be318d0
Choe Hyonsu Gabrielle <choehyonsu@gmail.com>
2022-05-03 10:30:31 +0900

    Update components.py
    minor refactoring

--- genetics/components.py
+++ genetics/components.py
@@ -24,26 +24,28 @@ class Individual:
         self.chromosome = chromosome
 
     def __repr__(self):
-        return f"<{self.__class__.__name__}> fitness: {self.fitness} / chromosome: {self.chromosome}"
+        return f"<{self.__class__.__name__} - fitness: {self.fitness} | chromosome: {self.chromosome}>"
 
 
 def reproduce(parents, reproduction_rate=2.0, mutation_rate=0.05):
     feasible_couples = list(permutations(parents, 2))
-    offsprings = []
     target_births = int(len(parents) * reproduction_rate)
+    offsprings = []
     while len(offsprings) < target_births:
         mom, dad = random.choice(feasible_couples)
-        elder, younger = one_point_crossover(mom.chromosome, dad.chromosome, mutation_rate=mutation_rate)
+        elder, younger = one_point_crossover(mom, dad, mutation_rate=mutation_rate)
         offsprings.extend([elder, younger])
     return offsprings
 
 
-def one_point_crossover(seq_a, seq_b, mutation_rate=0.05):
-    assert len(seq_a) == len(seq_b)
-    split_point = random.randrange(len(seq_a))
-    elder = Individual(fitness=0.0, chromosome=mutation(seq_a[:split_point] + seq_b[split_point:], rate=mutation_rate))
-    younger = Individual(fitness=0.0, chromosome=mutation(seq_b[:split_point] + seq_a[split_point:], rate=mutation_rate))
-    return elder, younger
+def one_point_crossover(parent_a, parent_b, mutation_rate=0.05):
+    assert len(parent_a.chromosome) == len(parent_b.chromosome)
+    split_point = random.randrange(len(parent_a.chromosome))
+    new_seq_a = mutation(seq_a[:split_point] + seq_b[split_point:], rate=mutation_rate)
+    new_seq_b = mutation(seq_b[:split_point] + seq_a[split_point:], rate=mutation_rate)
+    child_a = Individual(fitness=0.0, chromosome=new_seq_a)
+    child_b = Individual(fitness=0.0, chromosome=new_seq_b)
+    return child_a, child_b
 
 
 def mutation(seq, rate=0.05):
@@ -54,10 +56,10 @@ def mutation(seq, rate=0.05):
     return mutant
 
 
-class Generation:
+class GeneticAlgorithm:
     def __init__(self, config):
         self.config = config
-        self.gen = 0
+        self.generation = 0
         self.fitness_sum = 0.0
         self.population = []
 
@@ -68,7 +70,7 @@ class Generation:
         self.compute_fitness()
 
     def __repr__(self):
-        return f"<{self.__class__.__name__}> Gen: {self.gen} | Pop: {len(self.population)} | Fitness: {self.fitness_sum}"
+        return f"<{self.__class__.__name__} - Gen: {self.generation} | Pop: {len(self.population)} | FitSum: {self.fitness_sum}>"
 
     def forward(self):
         self.natural_selection()    # decide next generation individuals following POPULATION_SIZE
@@ -89,30 +91,29 @@ class Generation:
     def natural_selection(self):
         # selecting elites: they shall survive
         elites = self.population[:int(len(self.population) * self.config.ELITES_RATE)]
-        rest = self.population[int(len(self.population) * self.config.ELITES_RATE):]
         # selecting reproductive parents including elites
         fertile_parents = list(np.random.choice(self.population,
                                                 size=int(len(self.population) * self.config.FERTILE_PARENTS_RATE),
-                                                p=softmax([individual.fitness for individual in self.population])))
+                                                p=softmax([individual.fitness ** 2 for individual in self.population])))
         # offspring from fertile parents
         offsprings = reproduce(fertile_parents,
                                reproduction_rate=self.config.REPRODUCTION_RATE,
                                mutation_rate=self.config.MUTATION_RATE)
-        candidates = random.sample(fertile_parents + offsprings, k=self.config.POPULATION_SIZE - len(elites))
         # Replacing population with next generation individuals
+        candidates = random.sample(fertile_parents + offsprings, k=self.config.POPULATION_SIZE - len(elites))
         self.population = elites + candidates
         self.compute_fitness()
 
 
 if __name__ == '__main__':
     config = Config(data_size=100)
-    generation = Generation(config)
+    engine = GeneticAlgorithm(config)
 
     # for rank, entity in enumerate(generation.population):
     #     print(rank, entity.fitness, sum(entity.chromosome), entity.chromosome)
 
     for _ in range(100):
-        generation.forward()
+        engine.forward()
 
     # for rank, entity in enumerate(generation.population):
     #     print(rank, entity.fitness, sum(entity.chromosome), entity.chromosome)
